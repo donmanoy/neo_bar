a1e05a70db2d51695b6b1db0b5d384455163493d
Mohan Krishnan <mohangk@gmail.com>
2013-01-10 10:12:26 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NeoBar::Application.config.secret_token = 'bb625c42a95def1bb95a914b7ca6ac53ed4c1ba0bffee6870991ab577dcd9cbed3979483bf77de509dbcfa55ebbfa0c1b0d87c63493d767a9a39256708fceda3'
